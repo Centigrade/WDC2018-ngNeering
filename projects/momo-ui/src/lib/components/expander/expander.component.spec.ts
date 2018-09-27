25b72fec274997344aad66db8077d35a56ed138d
David Würfel <david.wuerfel@centigrade.de>
2018-09-27 15:29:03 +0200

    test: some input, output and state tests

--- projects/momo-ui/src/lib/components/expander/expander.component.spec.ts
+++ projects/momo-ui/src/lib/components/expander/expander.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ExpanderComponent } from './expander.component';
 
 describe('ExpanderComponent', () => {
@@ -17,7 +18,64 @@ describe('ExpanderComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  test('it should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  test('it should collapse the content if not expanded', () => {
+    // Act.
+    component.expanded = false;
+    fixture.detectChanges();
+
+    // Assert.
+    const content = fixture.debugElement.query(By.css('.content'));
+    expect(content).toEqual(null); // returns null if not available
+  });
+
+  test('it should show the content if expanded', () => {
+    // Act.
+    component.expanded = true;
+    fixture.detectChanges();
+
+    // Assert.
+    const content = fixture.debugElement.query(By.css('.content'));
+    expect(content).toBeTruthy();
+  });
+
+  test('it should render the title in the header', () => {
+    // Arrange.
+    const sampleTitle = 'Hello WDC';
+
+    // Act.
+    component.title = sampleTitle;
+    fixture.detectChanges();
+
+    // Assert.
+    const titleText: HTMLElement = fixture.debugElement.query(By.css('.title')).nativeElement;
+    expect(titleText.innerHTML).toEqual(sampleTitle);
+  });
+
+  test('it should set the disabled class on the host is disabled', () => {
+    // Arrange.
+    const hostElement = fixture.debugElement;
+
+    // Act.
+    component.disabled = true;
+    fixture.detectChanges();
+
+    // Assert.
+    expect(hostElement.classes.disabled).toBeTruthy();
+  });
+
+  test('it should not set the disabled class on the host is enabled', () => {
+    // Arrange.
+    const hostElement = fixture.debugElement;
+
+    // Act.
+    component.disabled = false;
+    fixture.detectChanges();
+
+    // Assert.
+    expect(hostElement.classes.disabled).toBeFalsy();
   });
 });
