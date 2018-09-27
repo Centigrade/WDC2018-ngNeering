b13bc1e48aed3888a935f2b2d59685c5445a464d
David Würfel <david.wuerfel@centigrade.de>
2018-09-27 09:35:26 +0200

    feat: theming example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'momo';
+
+  private themeToggled = false;
+
+  public toggleTheme() {
+    if (!this.themeToggled) {
+      this.setDarkTheme();
+    } else {
+      this.setLightTheme();
+    }
+
+    this.themeToggled = !this.themeToggled;
+  }
+
+  private setDarkTheme() {
+    window.document.body.style.setProperty(
+      '--brush-bg-highlight',
+      'linear-gradient(45deg, #2b7c42, #a3a524)',
+    );
+    window.document.body.style.setProperty('--brush-bg-light', '#000');
+    window.document.body.style.setProperty('--brush-fg-dark-bg', '#fff');
+  }
+
+  private setLightTheme() {
+    window.document.body.style.setProperty(
+      '--brush-bg-highlight',
+      'linear-gradient(45deg, #2b307c, #a72574)',
+    );
+    window.document.body.style.setProperty('--brush-bg-light', '#fff');
+    window.document.body.style.setProperty('--brush-fg-dark-bg', '#000');
+  }
 }
